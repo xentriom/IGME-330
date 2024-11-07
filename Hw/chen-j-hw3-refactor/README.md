47b20cddb484dc3d39e9749313c62ba313e246fa
Jason Chen <c04jason@gmail.com>
2024-11-07 10:07:49 -0500

    hw3 start
    fixed trailing slash to pass html validator

---
+++ Hw/chen-j-hw3-refactor/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">Audio Visualizer</h1>
---
+++ Hw/chen-j-hw3-refactor/src/utils.js
@@ -0,0 +1,65 @@
+const makeColor = (red, green, blue, alpha = 1) => {
+    return `rgba(${red},${green},${blue},${alpha})`;
+};
+
+const getRandom = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
+const getRandomColor = () => {
+    const floor = 35;
+    const getByte = () => getRandom(floor, 255 - floor);
+    return `rgba(${getByte()},${getByte()},${getByte()},1)`;
+};
+
+const getLinearGradient = (ctx, startX, startY, endX, endY, colorStops) => {
+    let lg = ctx.createLinearGradient(startX, startY, endX, endY);
+    for (let stop of colorStops) {
+        lg.addColorStop(stop.percent, stop.color);
+    }
+    return lg;
+};
+
+const goFullscreen = (element) => {
+    // fun fact: lint standards require else if to be in new line
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } 
+    else if (element.mozRequestFullscreen) {
+        element.mozRequestFullscreen();
+    } 
+    else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } 
+    else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    }
+};
+
+const setupSlider = (sliderId, labelId, unit, setValueCallback) => {
+    let slider = document.querySelector(sliderId);
+    let label = document.querySelector(labelId);
+
+    slider.oninput = e => {
+        setValueCallback(e.target.value);
+        label.innerHTML = `${e.target.value} ${unit}`;
+    };
+
+    slider.dispatchEvent(new Event("input"));
+};
+
+const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+
+    // format time as mm:ss
+    return `${String(minutes).padStart(1, '0')}:${String(secs).padStart(2, '0')}`;
+};
+
+// enum for toggle button images
+const TOGGLE_BUTTONS = Object.freeze({
+    PLAY: "https://c.animaapp.com/fgpR59jS/img/svgexport-35--1--1@2x.png",
+    PAUSE: "https://c.animaapp.com/yPlj5CdF/img/svgexport-83-1.svg"
+})
+
+export { makeColor, getRandomColor, getLinearGradient, goFullscreen, setupSlider, formatTime, TOGGLE_BUTTONS };
