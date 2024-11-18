05522084a1d6314a95ad0e0893a95ce530c6e139
Jason <c04jason@gmail.com>
2024-11-18 01:29:27 -0500

    hw4 starter

--- Pe/chen-j-hw4-starter/src/main.js
+++ Pe/chen-j-hw4-starter/src/main.js
@@ -5,6 +5,7 @@ import * as ajax from "./ajax.js";
 // NB - it's easy to get [longitude,latitude] coordinates with this tool: http://geojson.io/
 const lnglatNYS = [-75.71615970715911, 43.025810763917775];
 const lnglatUSA = [-98.5696, 39.8282];
+let favoriteIds = ["p20", "p79", "p180", "p43"];
 let geojson;
 
 // II. Functions
@@ -30,6 +31,7 @@ const setupUI = () => {
 		map.flyTo(lnglatUSA);
 	};
 
+	refreshFavorites();
 };
 
 const showFeatureDetails = (id) => {
@@ -45,12 +47,42 @@ const showFeatureDetails = (id) => {
 };
 
 const getFeatureById = (id) => {
-    return geojson.features.find(feature => feature.id === id);
+	return geojson.features.find(feature => feature.id === id);
+};
+
+const refreshFavorites = () => {
+	const favoritesContainer = document.querySelector("#favorites-list");
+	favoritesContainer.innerHTML = "";
+
+	for (const id of favoriteIds) {
+		favoritesContainer.appendChild(createFavoriteElement(id));
+	}
+};
+
+const createFavoriteElement = (id) => {
+	const feature = getFeatureById(id);
+	const a = document.createElement("a");
+
+	a.className = "panel-block";
+	a.id = feature.id;
+
+	a.onclick = () => {
+		showFeatureDetails(a.id);
+		map.setZoomLevel(6);
+		map.flyTo(feature.geometry.coordinates);
+	};
+
+	a.innerHTML = `
+		<span class="panel-icon">
+			<i class="fas fa-map-pin"></i>
+		</span>
+		${feature.properties.title}
+	`;
+	return a;
 };
 
 const init = () => {
 	map.initMap(lnglatNYS);
-	setupUI();
 	ajax.downloadFile("data/parks.geojson", (str) => {
 		geojson = JSON.parse(str);
 		console.log(geojson);
