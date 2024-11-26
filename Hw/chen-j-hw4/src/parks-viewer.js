501288ab0823ec08af64d6652e9067096df22563
Jason <c04jason@gmail.com>
2024-11-26 00:48:16 -0500

    prettied up viewer html, base code for viewer js

--- Hw/chen-j-hw4/src/parks-viewer.js
+++ Hw/chen-j-hw4/src/parks-viewer.js
@@ -1,9 +1,22 @@
-const refreshData = () => {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase, ref, set, push, onValue } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+
+let app;
+
+const initApp = () => {
+    const firebaseConfig = {
+
+    };
+    app = initializeApp(firebaseConfig);
+};
+
+const loadData = () => {
 
 };
 
 const init = () => {
-
+    initApp();
+    loadData();
 };
 
 init();
